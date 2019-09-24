f2d8e56d673a7049c41ec3db49244be7d5ee1d53
Sophie Crommelinck <sophie.crommelinck@gmail.com>
2019-09-24 10:15:39 +0200

    Update generateTrainingdata.py

--- v3.0/2_boundary_classification/Convolutional Neural Network Classification/generateTrainingdata.py
+++ v3.0/2_boundary_classification/Convolutional Neural Network Classification/generateTrainingdata.py
@@ -1,26 +1,35 @@
-# -*- coding: utf-8 -*-
 """
-/***************************************************************************
-
-                              -------------------
+!/bin/python
+-*- coding: utf-8 -*-
+/*****************************************************************************
         begin                : 2019-03-25
-        copyright            : (C) 2019 by Sophie Crommelinck, University of Twente
+        copyright            : (C) 2019 by Sophie Crommelinck, 
+                                University of Twente
         email                : s.crommelinck@utwente.nl
-        description          : this script loops over all features of given shapefile 
-				and saves clipped rasters around each line's pixel 
-				according to its boundary label in a specific folder.
-        funding               H2020 EU project its4land (#687826, its4land.com)
+        description          : this script loops over all features of given 
+				shapefile and saves clipped rasters around 
+				each line's pixel according to its boundary 
+				label in a specific folder.
+        funding              : H2020 EU project its4land 
+                                (#687826, its4land.com)
                                 Work package 5: Automate It
- ***************************************************************************/
-
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
+	development          : Sophie Crommelinck
+ *****************************************************************************/
+ 
+ /*****************************************************************************
+ *    This program is free software: you can redistribute it and/or modify    *
+ *    it under the terms of the GNU General Public License as published by    *
+ *    the Free Software Foundation, either version 3 of the License, or       *
+ *    (at your option) any later version.                                     *
+ *                                                                            *
+ *    This program is distributed in the hope that it will be useful,         *
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *    GNU General Public License for more details.                            *
+ *                                                                            *
+ *    You should have received a copy of the GNU General Public License       *
+ *    along with this program.  If not, see <https://www.gnu.org/licenses/>.  *
+  *****************************************************************************/
 """
 
 # Import modules
