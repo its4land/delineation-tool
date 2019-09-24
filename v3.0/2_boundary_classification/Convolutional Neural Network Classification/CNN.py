b87e9f1e633b56cf6b53cc3da912c195214c69ff
Sophie Crommelinck <sophie.crommelinck@gmail.com>
2019-09-24 10:12:16 +0200

    Update CNN.py

--- v3.0/2_boundary_classification/Convolutional Neural Network Classification/CNN.py
+++ v3.0/2_boundary_classification/Convolutional Neural Network Classification/CNN.py
@@ -1,27 +1,37 @@
-# -*- coding: utf-8 -*-
 """
-/***************************************************************************
-its4land - Automate It
-                               -------------------
+!/bin/python
+-*- coding: utf-8 -*-
+/*****************************************************************************
         begin                : 2019-03-22
-        git sha              : $Format:%H$
-        copyright            : (C) 2019 by Sophie Crommelinck, University of Twente
+        copyright            : (C) 2019 by Sophie Crommelinck, 
+                                University of Twente
         email                : s.crommelinck@utwente.nl
         description          : this script applies transfer-learning on a
                                 pre-trained CNN to predict class
-                                probabilities for image tiles
-        funding              : H2020 EU project its4land WP5 Automate It
- ***************************************************************************/
-
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
+                                probabilities for image tiles.
+        funding              : H2020 EU project its4land 
+                                (#687826, its4land.com)
+                                Work package 5: Automate It
+		    development          : Reiner Borchert, Hansa Luftbild AG Münster
+        email                : borchert@hansaluftbild.de
+ *****************************************************************************/
+ 
+ /*****************************************************************************
+ *    This program is free software: you can redistribute it and/or modify    *
+ *    it under the terms of the GNU General Public License as published by    *
+ *    the Free Software Foundation, either version 3 of the License, or       *
+ *    (at your option) any later version.                                     *
+ *                                                                            *
+ *    This program is distributed in the hope that it will be useful,         *
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *    GNU General Public License for more details.                            *
+ *                                                                            *
+ *    You should have received a copy of the GNU General Public License       *
+ *    along with this program.  If not, see <https://www.gnu.org/licenses/>.  *
+  *****************************************************************************/
 """
+
 # Import modules
 from keras import backend as K
 from keras import utils
